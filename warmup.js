8fdb2f88d663393e11111693cbe8e0a7be650302
Abdo Andic <abdo.andic@gmail.com>
2018-02-16 10:38:27 -0500

    initial

---
+++ warmup.js
@@ -0,0 +1,11 @@
+var arrayMe = ["total recall", 31, "istanbul", true, 1911, null];
+console.log(arrayMe);
+
+arrayMe.push("bamboo");
+console.log(arrayMe);
+
+arrayMe.splice(1,2, "alex")
+console.log(arrayMe);
+
+arrayMe.unshift("me first");
+console.log(arrayMe);
