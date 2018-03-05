4cab7df4e94bb35f7fbda04f64e7d21f5a5043a0
Abdo Andic <abdo.andic@gmail.com>
2018-03-05 11:14:28 -0500

    initial commit

--- warmup.js
+++ warmup.js
@@ -1,11 +1,11 @@
-var arrayMe = ["total recall", 31, "istanbul", true, 1911, null];
+var arrayMe = ["total recall", 31, "new york", true, 1911, null];
 console.log(arrayMe);
 
 arrayMe.push("bamboo");
 console.log(arrayMe);
 
-arrayMe.splice(1,2, "alex")
+arrayMe.splice(1,2, "abdo")
 console.log(arrayMe);
 
-arrayMe.unshift("me first");
+arrayMe.unshift("me last");
 console.log(arrayMe);
